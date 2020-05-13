98afdd32ac40986bd0d3c7ff6c7418b2287822ac
Stewart Cossey <stewart.cossey@gmail.com>
2020-05-14 02:25:16 +1200

    Remove Acarda Outbound automation.

--- main.ps1
+++ main.ps1
@@ -5,6 +5,5 @@ while ($true) {
     Write-Host "-----------------------------"
     Write-Host "Running at $(Get-Date)"
     Invoke-Expression -Command "${PSScriptRoot}/burnaware.ps1 ${datapath}/burnaware.ver"
-    Invoke-Expression -Command "${PSScriptRoot}/acardaoutbound.ps1 ${datapath}/aos.ver ${datapath}/aot.ver"
     Start-Sleep -Seconds ([int]$Env:DELAY * 60)
 }
