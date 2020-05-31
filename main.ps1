94303bc0fcd7eb9f83b117e9e34b72f7668440b6
Stewart Cossey <stewart.cossey@gmail.com>
2020-05-31 18:33:30 +1200

    Move line divider to bottom of script.

--- main.ps1
+++ main.ps1
@@ -7,13 +7,13 @@ $progressPreference = 'silentlyContinue'
 . "${PSScriptRoot}/common.ps1"
 
 while ($true) {
-    Write-Host "-----------------------------"
     Write-Host "Running at $(Get-Date)"
-
+    
     & ${PSScriptRoot}/burnaware.ps1
     & ${PSScriptRoot}/rpcs3.ps1
     & ${PSScriptRoot}/roccatswarm.ps1
-
+    
     if (-not (Test-Path env:DELAY)) {break}
     Start-Sleep -Seconds ([int]$Env:DELAY * 60)
+    Write-Host "-----------------------------"
 }
