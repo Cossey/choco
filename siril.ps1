1280c2fdc2998ea441d9ae742a8d83f349eabd37
Stewart Cossey <stewart.cossey@gmail.com>
2022-09-12 02:14:43 +1200

    Update SIRIL
    Also add vscode launch file.

--- siril.ps1
+++ siril.ps1
@@ -1,25 +1,21 @@
 if (InitPackage("siril")) {
-    SetRootURL "https://free-astro.org"
-    $releasesurl = PrefixRootURL "/index.php?title=Siril:releases"
+    SetRootURL "https://siril.org"
+    $releasesurl = PrefixRootURL "/download/"
     
-    if (LinkList $releasesurl "Siril:[0-9.]+") {
+    if (LinkList $releasesurl ".*-siril-.*") {
         $releaseurl = GetLinkList 0
     
-        if (SetVersion([regex]::match($releaseurl, "Siril:([0-9.]*)").Groups[1].Value)) {
-            $releaseinfo = ObtainReleaseInfo $releaseurl "<b>Release date: (?<date>.*?)</b>.*What?'s new.*?</h2>(?<info>.*?)(<!--|</div>)" $true
+        if (SetVersion([regex]::match($releaseurl, ".*-siril-(.*?)/").Groups[1].Value)) {            
+            $releaseinfo = ObtainReleaseInfo $releaseurl ".*?released on\s+(?<date>.*?)\..*id=`"whats-new`".*?</h[1-3]>(?<info>.*?)<h[1-3]" $true
 
             if (LinkList $releaseurl) {
-                $dlpage = GetLinkList ".*/download/.*"
+                $windowsdownload = GetLinkList ".*\.exe$"
 
-                if (LinkList $dlpage) {
-                    $windowsdownload = GetLinkList ".*\.exe$"
-
-                    $installer64 = DownloadInstallerFile $windowsdownload 
-                    if (IncludeEULA "https://gitlab.com/free-astro/siril/-/raw/master/LICENSE.md") {
-                        if (CompileTemplates $null $installer64 $releaseinfo) {
-                            if (PackAndClean) {
-                                PackageUpdated $size
-                            }
+                 
+                if (IncludeEULA "https://gitlab.com/free-astro/siril/-/raw/master/LICENSE.md") {
+                    if (CompileTemplates $null $installer64 $releaseinfo) {
+                        if (PackAndClean) {
+                            PackageUpdated $size
                         }
                     }
                 }
